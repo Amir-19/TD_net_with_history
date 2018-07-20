1891e632e95d08a2e497c6cbbd2567a01a820fe6
Amir Samani <amir.samani07@icloud.com>
2018-07-20 14:05:25 -0600

    starting day 2 of debuging

--- settings.py
+++ settings.py
@@ -1,10 +1,11 @@
 # settings for the experiment
 
+
 class Settings(object):
 
     step_size = 0.1
     history_length_observation = 6
     history_length_action = 6
     question_network_layer = 5
-    activation_function = "sigmoid" # the other option is "identity"
+    activation_function = "sigmoid"  # the other option is "identity"
     training_steps = 50000000
